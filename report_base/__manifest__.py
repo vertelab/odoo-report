db9af92d32458cc8eae7b1f8caf1e543e3332e27
Jakob Krabbe <jakob.krabbe@vertel.se>
2022-05-24 13:03:45 +0000

    Fixed __manifest__

--- report_base/__manifest__.py
+++ report_base/__manifest__.py
@@ -28,7 +28,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Reporting',
-    'description': ''.
+    'description': '',
     #'sequence': '1'
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-report/report-base',
