bafdc75ec8c5e5ecabb9a38e23cdae8372dcf812
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-15 10:48:15 +0200

    Update __manifest__.py

--- report_scribus/__manifest__.py
+++ report_scribus/__manifest__.py
@@ -27,11 +27,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Reporting',
+    'category': 'Project',
     'description': '',
     #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-report/report-scribus',
+    'website': 'https://vertel.se/apps/odoo-report/report_scribus',
     'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
