0fa077bf0070efc51d03a64d2e552514cb1236dd
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-15 10:46:25 +0200

    Update __manifest__.py

--- report_glabels/__manifest__.py
+++ report_glabels/__manifest__.py
@@ -27,7 +27,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Reporting',
+    'category': 'Project',
     'description': '',
     #'sequence': '1'
     'author': 'Vertel AB',
