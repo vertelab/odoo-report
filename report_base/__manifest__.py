58f9fcf13fb17d73358f31603ab889e4b3ca7994
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-03-09 11:58:57 +0100

    Update __manifest__.py
    minor spelling details

--- report_base/__manifest__.py
+++ report_base/__manifest__.py
@@ -20,10 +20,10 @@
 ##############################################################################
 
 {
-    'name': 'Report: Report Base',
+    'name': 'Report: Base',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': 'Adds report type for glabel',
+    'summary': 'Adds report type for glabel.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
