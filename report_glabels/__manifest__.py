3e28b6ed9abaa5d0f065d4e2bf7525e7b5e8bebe
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-03-09 11:59:32 +0100

    Update __manifest__.py
    minor spelling details

--- report_glabels/__manifest__.py
+++ report_glabels/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Report: Report Glabels',
+    'name': 'Report: Glabels',
     'version': '14.0.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Add features to gLabel.',
