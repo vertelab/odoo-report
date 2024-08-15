301f9b8ffe57370c04999e3a1fa9f4a5f33bd067
marcuszxc <marcuszxc10@gmail.com>
2024-08-15 10:02:44 +0000

    odoobranchpfile report_glabels from 13.0

--- report_glabels/__manifest__.py
+++ report_glabels/__manifest__.py
@@ -1,30 +1,22 @@
-# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "Report Glabels",
-
-    'summary': """
-        Report system for glabels in odoo
-    """,
-    'description': """
-        Report system for glabels in odoo
-    """,
-
-    'author': "Vertel AB",
-    'website': "vertel.se",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'report',
-    'version': '13.0.0.0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'report_base'],
-    'external_dependencies': {'python': ['csv',], 'bin': ['glabels-3-batch']},
-
-    # always loaded
-    'data': [
-        'views/report_view.xml',
-    ],
-    'sequence' : 5
-}
+    'name': 'Report: Glabels',
