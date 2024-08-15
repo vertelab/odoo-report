8414589ff0156b997c27699ca5726a7a3e18626a
marcuszxc <marcuszxc10@gmail.com>
2024-08-15 11:01:23 +0000

    odoobranchpfile report_glabels from 13.0

--- report_glabels/__manifest__.py
+++ report_glabels/__manifest__.py
@@ -20,3 +20,29 @@
 
 {
     'name': 'Report: Glabels',
+    'summary': 'Add features to gLabel.',
+    'category': 'Project',
+    'description': """
+    Add fields to gLabel. Add template, count, column name and column value.
+    """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-report/report_glabels',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-report',
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'report_base'],
+    'external_dependencies': {'python': ['csv',], 'bin': ['glabels-3-batch']},
+    'data': [
+        'views/report_view.xml',
+        #"wizard/report_test.xml",
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'sequence' : 5
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
--- report_glabels/models/report_xml.py
+++ report_glabels/models/report_xml.py
@@ -18,3 +18,10 @@ class IrActionsReport(models.Model):
 
     def _get_csv_fields(self):
         self.csv_fields = ','.join(sorted(self.env[self.model]._fields.keys()))
+    glabels_template = fields.Binary(string="Glabels template")
+    label_count = fields.Integer(string="Count", default=1,help = "One if you want to fill the sheet with new records, the count of labels of the sheet to fill each sheet with one record")
+    col_name = fields.Char(string="Column", help = "(Glabels rows) the name of name column for use in gLabels")
+    col_value = fields.Char(string="Column", help = "(Glabels rows) the name of value column for use in gLabels")
+    csv_fields = fields.Text(compute="_get_csv_fields")
+    
+
