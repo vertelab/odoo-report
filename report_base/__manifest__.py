b0f2aec8cc9afe41031c25ab4d53c7d1eb9a9c29
marcuszxc <marcuszxc10@gmail.com>
2024-08-19 14:35:01 +0000

    odoobranchpfile report_base from 12.0

--- report_base/__manifest__.py
+++ report_base/__manifest__.py
@@ -1,29 +1,42 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "report_base",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': 'Report: Base',
+    'summary': 'Adds report type for glabel.',
+    'category': 'Project',
     'description': """
-        Long description of module's purpose
+    Adds report type to back-end.
     """,
-
-    'author': "Vertel AB",
-    'website': "vertel.se",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '12.0.0.0.1',
-
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-report/report_base',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-report',
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-     #always loaded
-     'data': [
-        'views/views.xml',
+    # always loaded
+    'data': [
     ],
 }
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
