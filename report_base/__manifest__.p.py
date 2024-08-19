89bf05426292bb69d84e8e7b830e337df9ae20e2
marcuszxc <marcuszxc10@gmail.com>
2024-08-19 14:17:33 +0000

    odoobranchpfile report_base from 12.0

---
+++ report_base/__manifest__.p.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Report: Base',
+    # #if VERSION >= "16.0"
+    'version': '1.1',
+    # #elif VERSION == "14.0"
+    'version': '1.0',
+    # #endif
+    'summary': 'Adds report type for glabel.',
+    'category': 'Project',
+    'description': """
+    Adds report type to back-end.
+    """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-report/report_base',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-report',
+    # any module necessary for this one to work correctly
+    # #if VERSION >= "16.0"
+    'depends': ['base_setup'],
+    'external_dependencies': {'python': ['unicodecsv']},
+    # #elif VERSION == "14.0"
+    'depends': ['base'],
+    # #endif
+    # always loaded
+    'data': [
+        # #if VERSION >= "16.0"
+        #'views/views.xml',
+        'views/act_report_xml_view.xml'
+        # #elif VERSION == "14.0"
+        'views/views.xml',
+        # #endif
+    ],
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
