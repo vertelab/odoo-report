996e8f25d1afbc7c2f0addbb47e8cbaff55f8cd8
Jakob Krabbe <jakob@syscare.se>
2024-08-26 09:30:59 +0000

    report_scribus: just some minor manifest text, 14.0 >> 17.0

--- report_scribus/__manifest__.py
+++ report_scribus/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Report: Scribus Reports',
-    'version': '14.0.0.0.1',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '0.1',
+    # Version ledger: XX.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Reports for Scribus publishing system',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
