c9b4c131e6d5c49e91edf0c4f339c8265ec02883
iammiracle <miracleaayodele@gmail.com>
2025-07-01 12:03:07 +0200

    [MIG][18.0] report_base, report_glabels

--- report_base/models/ir_action_report.p.py
+++ report_base/models/ir_action_report.p.py
@@ -1,4 +1,4 @@
-# #if VERSION >= "16.0"
+# #if VERSION >= "17.0"
 # #if VERSION == "16.0"
 from odoo.exceptions import except_orm, Warning, RedirectWarning
 # #endif
@@ -17,7 +17,11 @@ class IrActionsReport(models.Model):
     
     _inherit = 'ir.actions.report'
 
-    fake_report_type = fields.Selection([('qweb-html', 'HTML'),('qweb-pdf', 'PDF'),('qweb-text', 'Text')], string="Report Type", required=True, default='qweb-pdf',
+    fake_report_type = fields.Selection([
+        ('qweb-html', 'HTML'),
+        ('qweb-pdf', 'PDF'),
+        ('qweb-text', 'Text')
+    ], string="Report Type", required=True, default='qweb-pdf',
     help='The type of the report that will be rendered, each one having its own'
         ' rendering method. HTML means the report will be opened directly in your'
         ' browser PDF means the report will be rendered using Wkhtmltopdf and'
